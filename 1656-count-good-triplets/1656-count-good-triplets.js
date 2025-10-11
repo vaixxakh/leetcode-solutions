368d5e1c97b7a9b38f1998571ff29450d2bc0b49
Vaishakh Vinod <157811409+vaixxakh@users.noreply.github.com>
2025-10-11 10:04:11 +0530

    Time: 61 ms (22.04%), Space: 53.4 MB (95.16%) - LeetHub

---
+++ 1656-count-good-triplets/1656-count-good-triplets.js
@@ -0,0 +1,21 @@
+const countGoodTriplets = (arr, a, b, c) => {
+  let count = 0;
+
+        for (let i = 0; i < arr.length - 2; i++) {
+        for (let j = i + 1; j < arr.length - 1; j++) {
+        for (let k = j + 1; k < arr.length; k++) {
+     if (
+          Math.abs(arr[i] - arr[j]) <= a &&
+          Math.abs(arr[j] - arr[k]) <= b &&
+          Math.abs(arr[i] - arr[k]) <= c
+         ){
+          count++;
+        }
+        }
+        }
+        }
+
+  return count;
+};
+
+console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3));
